0ebe24da83faee00cf407018401f1616d4e62c69
nguynlinhchi <nguyenlinhchi2019@gmail.com>
2025-03-25 05:23:08 +0700

    Update dangnhap_main.py

--- dangnhap_main.py
+++ dangnhap_main.py
@@ -1,5 +1,5 @@
 from PyQt6.QtWidgets import QApplication, QMainWindow
-from dangnhap_extend import dangnhap_ext #Gọi lớp kế thừa
+from dangnhap_extend import dangnhap_ext
 import sys
 
 app = QApplication.instance()
@@ -7,7 +7,6 @@ if app is None:
     app = QApplication(sys.argv)
 
 w = QMainWindow()
-# f = Ui_MainWindow
 f = dangnhap_ext()
 f.setupUi(w)
 
