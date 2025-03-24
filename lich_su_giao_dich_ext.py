b1cb6e980e841ba6745137591f4dff3f5e8f3968
thuy-trang-63 <trangntt23406@st.uel.edu.vn>
2025-03-25 03:00:18 +0700

    Add files via upload

--- lich_su_giao_dich_ext.py
+++ lich_su_giao_dich_ext.py
@@ -1,19 +1,21 @@
 import json
 
-from PyQt6.QtWidgets import QMessageBox, QTableWidget, QTableWidgetItem
-
+from PyQt6.QtWidgets import QMessageBox, QTableWidget, QTableWidgetItem, QMainWindow
+from PyQt6.QtCore import QDate
 from lich_su_giao_dich_mainwindow import Ui_MainWindow
 class Ui_MainWindow_ext (Ui_MainWindow):
     def setupUi(self, MainWindow):
         super().setupUi(MainWindow)
         self.MainWindow = MainWindow
-        self.tblLichSuGiaoDich.setHorizontalHeaderLabels(["Tên giao dịch","Số tiền","Ngày thực hiện","Ghi chú"])
+        self.tblLichSuGiaoDich.setHorizontalHeaderLabels(["Ghi chú","Số tiền","Ngày thực hiện","Loại giao dịch"])
         self.tblLichSuGiaoDich.setColumnWidth(0, 200)
         self.tblLichSuGiaoDich.setColumnWidth(1, 200)
         self.tblLichSuGiaoDich.setColumnWidth(2, 200)
         self.tblLichSuGiaoDich.setColumnWidth(3, 200)
         self.txtNgayBatDau.setCalendarPopup(True)
+        self.txtNgayBatDau.setDate(QDate.currentDate())
         self.txtNgayKetThuc.setCalendarPopup(True)
+        self.txtNgayKetThuc.setDate(QDate.currentDate())
         self.txtTongChiTieu.setReadOnly(True)
         self.txtTongChiTieu.setPlaceholderText("0 VND")
 
@@ -22,6 +24,17 @@ class Ui_MainWindow_ext (Ui_MainWindow):
 
         #ấn cộng tiền
         self.btnTongchiTieu.clicked.connect (self.tong_chi_tieu)
+
+        #ấn quay lại
+        self.btnQuayLai.clicked.connect(self.quay_lai)
+
+    def quay_lai(self):
+        from Thong_ke_bao_cao_ext import Thong_ke_bao_cao_ext
+        self.MainWindow.close()
+        self.thong_ke_bao_cao=QMainWindow()
+        self.ui_thong_ke_bao_cao=Thong_ke_bao_cao_ext()
+        self.ui_thong_ke_bao_cao.setupUi(self.thong_ke_bao_cao)
+        self.thong_ke_bao_cao.show()
 
     def tong_chi_tieu(self):
         tong_tien = 0
@@ -39,36 +52,44 @@ class Ui_MainWindow_ext (Ui_MainWindow):
 
     def kiem_tra_loai_giao_dich (self):
         if self.txtLoaiGiaoDich.currentText() == "Loại giao dịch":
-            self.btnTimKiem.setEnabled(False)
             QMessageBox.information(self.MainWindow, "Thông báo", "Bạn chưa chọn loại giao dịch")
             return False
         return True
 
     def kiem_tra_ngay(self):
         if self.txtNgayBatDau.text()> self.txtNgayKetThuc.text():
-            # self.btnTimKiem.setEnabled(False)
             QMessageBox.information(self.MainWindow, "Thông báo", "Ngày kết thúc phải sau ngày bắt đầu")
             return False
         return True
 
     def cap_nhat_du_lieu (self):
-        with open ("data/muctieutc.json", "r", encoding="utf-8") as file:
+        with open ("data/khoanchi.json", "r", encoding="utf-8") as file:
             try:
                 lich_su_giao_dich = json.load(file)
             except:
                 lich_su_giao_dich = []
+        giao_dich_ton_tai = []
+        giao_dich_theo_ngay=[]
         for giao_dich in lich_su_giao_dich:
-            giao_dich_ton_tai=[]
-            if self.txtNgayBatDau.text() <= giao_dich["ngày"] <= self.txtNgayKetThuc.text():
-                giao_dich_ton_tai.append(giao_dich)
-                self.tblLichSuGiaoDich.setRowCount(len(giao_dich_ton_tai))
-                for row, giao_dich in enumerate (giao_dich_ton_tai):
-                    self.tblLichSuGiaoDich.setItem(row, 0, QTableWidgetItem(giao_dich["khoản thu"]))
-                    self.tblLichSuGiaoDich.setItem(row, 1, QTableWidgetItem(giao_dich["số tiền"]))
-                    self.tblLichSuGiaoDich.setItem(row, 2, QTableWidgetItem(giao_dich["ngày"]))
-                    self.tblLichSuGiaoDich.setItem(row, 3, QTableWidgetItem(giao_dich["ghi chú"]))
-            else:
-                QMessageBox.information(self.MainWindow,"Thông báo","Không có giao dịch nào trong khoảng thời gian này")
+            ngay_bat_dau = self.txtNgayBatDau.date().toString("dd/MM/yyyy")
+            ngay_ket_thuc = self.txtNgayKetThuc.date().toString("dd/MM/yyyy")
+
+            if ngay_bat_dau <= giao_dich["ngày"] <= ngay_ket_thuc:
+                giao_dich_theo_ngay.append(giao_dich)
+                if self.txtLoaiGiaoDich.currentText() == "Tất cả":
+                    giao_dich_ton_tai=giao_dich_theo_ngay
+                else:
+                    if giao_dich.get("khoản chi")==self.txtLoaiGiaoDich.currentText():
+                        giao_dich_ton_tai.append(giao_dich)
+        if len(giao_dich_ton_tai)==0:
+            QMessageBox.information(self.MainWindow, "Thông báo", "Không tìm được ngày hoặc loại giao dịch phù hợp")
+        else:
+            self.tblLichSuGiaoDich.setRowCount(len(giao_dich_ton_tai))
+            for row, giao_dich in enumerate (giao_dich_ton_tai):
+                self.tblLichSuGiaoDich.setItem(row, 0, QTableWidgetItem(giao_dich["ghi chú"]))
+                self.tblLichSuGiaoDich.setItem(row, 1, QTableWidgetItem(giao_dich["số tiền"]))
+                self.tblLichSuGiaoDich.setItem(row, 2, QTableWidgetItem(giao_dich["ngày"]))
+                self.tblLichSuGiaoDich.setItem(row, 3, QTableWidgetItem(giao_dich["khoản chi"]))
 
 
 
