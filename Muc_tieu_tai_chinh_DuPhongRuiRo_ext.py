4222eb7935c5e051db4e5d7b2042ce5f0e414db2
thuy-trang-63 <trangntt23406@st.uel.edu.vn>
2025-03-25 07:13:34 +0700

    Update Muc_tieu_tai_chinh_DuPhongRuiRo_ext.py

--- Muc_tieu_tai_chinh_DuPhongRuiRo_ext.py
+++ Muc_tieu_tai_chinh_DuPhongRuiRo_ext.py
@@ -92,8 +92,8 @@ class Ui_DuPhongRuiRo_Ext(Ui_DuPhongRuiRo):
         return True
 
     def kiem_tra_tien(self):
-        if self.txtSoTienMucTieu.text() == "":
-            QMessageBox.warning(self.du_phong_rui_ro_mw, "Cảnh báo", "Hãy điền số tiền mục tiêu")
+        if self.txtSoTienMucTieu.text() == "" or not self.txtSoTienMucTieu.text().isdigit():
+            QMessageBox.warning(self.an_uong_vui_ve_mw, "Cảnh báo", "Số tiền phải là 1 số")
             return False
         return True
 
