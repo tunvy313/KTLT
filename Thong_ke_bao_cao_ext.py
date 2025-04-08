f698f0d28ac8145555825c7ead404f283cda425f
nguynlinhchi <nguyenlinhchi2019@gmail.com>
2025-04-08 15:46:08 +0700

    Update Thong_ke_bao_cao_ext.py

--- Thong_ke_bao_cao_ext.py
+++ Thong_ke_bao_cao_ext.py
@@ -5,11 +5,14 @@ class Thong_ke_bao_cao_ext(QMainWindow,Ui_thong_ke_bao_cao):
     def setupUi(self, thong_ke_bao_cao_mw):
         super().setupUi(thong_ke_bao_cao_mw)
         self.thong_ke_bao_cao_mw = thong_ke_bao_cao_mw
+        self.btnQuayLai.clicked.connect(self.process_quaylai)
 
     #mở lịch sử giao dịch
         self.btnLSGD.clicked.connect(self.mo_lich_su_giao_dich)
     #mở mục tiêu tài chính
         self.btnMTTC.clicked.connect(self.mo_muc_tieu_tai_chinh)
+    #mở báo cáo tài chính
+        self.btnBCTC.clicked.connect(self.mo_bct_chinh)
 
     def mo_lich_su_giao_dich(self):
         from lich_su_giao_dich_ext import Ui_MainWindow_ext
@@ -26,3 +29,20 @@ class Thong_ke_bao_cao_ext(QMainWindow,Ui_thong_ke_bao_cao):
         self.ui_muc_tieu_tai_chinh_ext.setupUi(self.muc_tieu_tai_chinh)
         self.muc_tieu_tai_chinh.show()
         self.thong_ke_bao_cao_mw.close()
+
+    def mo_bct_chinh(self):
+        from phantich_ext import phantich_ext
+        self.phantich = QMainWindow()
+        self.phantich_ext = phantich_ext()
+        self.phantich_ext.setupUi(self.phantich)
+        self.phantich.show()
+        self.thong_ke_bao_cao_mw.close()
+
+    def process_quaylai(self):
+        # Quay lại màn hình chính
+        from manhinhchinh_extend import manhinhchinh_ext
+        self.manhinhchinh_form = QMainWindow()
+        self.ui_manhinhchinh = manhinhchinh_ext()
+        self.ui_manhinhchinh.setupUi(self.manhinhchinh_form)
+        self.thong_ke_bao_cao_mw.close()  # Đóng cửa sổ hiện tại
+        self.manhinhchinh_form.show()  # Hiển thị màn hình chính
